04257ea6b9deb6de6f34692e3efc60461c53ac4d
sunlong <2013855675@qq.com>
2017-03-23 23:54:28 +0800

    '11'

--- Apps/Home/Controller/BaseController.class.php
+++ Apps/Home/Controller/BaseController.class.php
@@ -66,18 +66,6 @@ class BaseController extends Controller {
 		$this->assign('navList',$nav);
 	}
 
-    public function get_array($id=0){
-        $sql = "select id,title from class where pid= $id";
-        $result = mysql_query($sql);//查询子类
-        $arr = array();
-        if($result && mysql_affected_rows()){//如果有子类
-            while($rows=mysql_fetch_assoc($result)){ //循环记录集
-                $rows['list'] = get_array($rows['id']); //调用函数，传入参数，继续查询下级
-                $arr[] = $rows; //组合数组
-            }
-            return $arr;
-        }
-    }
     /**
 	 * 获取页面页脚内容
 	 */
