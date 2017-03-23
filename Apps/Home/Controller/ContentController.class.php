dc383c33e7e313facc49dc0f90f7289362155898
taoyu <taoyu>
2017-03-24 02:34:12 +0800

    ty

---
+++ Apps/Home/Controller/ContentController.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Home\Controller;
+
+class ContentController extends BaseController {
+
+	public function index()
+    {
+        if (empty($_GET['id'])) {
+            return $this->error("缺少指定参数!");
+        }
+
+        $id = intval($_GET['id']);
+        $contentModel = D ('Content');
+        $content = $contentModel->find($id);
+
+        if (is_null($content)) {
+            $this->error("您要访问的内容不存在!");
+        }
+
+
+    }
+}
