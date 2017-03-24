11c0dd44b43c9c2a684e42ae5ceed641ce0e4bd9
sunlong <2013855675@qq.com>
2017-03-25 00:07:14 +0800

    完善搜索功能

--- Apps/Home/Controller/SearchController.class.php
+++ Apps/Home/Controller/SearchController.class.php
@@ -15,8 +15,9 @@ class SearchController extends BaseController
     protected $limitPage = 10;
     public function search($keyword = '')
     {
+
         $content = D('Content');
-        //这里待会验证
+
         $kewword = I('get.keyword', '', 'strip_tags,stripslashes');
 
         // 分页处理,获取数据
@@ -25,9 +26,9 @@ class SearchController extends BaseController
         $Page = new MyPage( $count, $this->limitPage );
         $newResult = array_slice($this->result,$Page->firstRow,$Page->listRows);
         $show = $Page->show ();
-
+        $this->setTitle('文章搜索');
         $this->assign ('page', $show);
         $this->assign('searchResult',$newResult);
-        $this->display('list');
+        $this->display('index');
     }
 }
